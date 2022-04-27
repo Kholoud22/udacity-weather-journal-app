7569274f500cc45bd60f5d8970c73011bbb475a1
Kholoud Mohamed <khlood_mohamed@hotmail.com>
2022-04-27 23:17:40 +0200

    update port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('website'));
 
 
 // Setup Server
-const port = 3000;
+const port = 443;
 
 const server = app.listen(port, () => console.log(`server running on localhost: ${port}`));
 
